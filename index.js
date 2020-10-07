78e312a6c1b0924a6a9fecefbb6d9c0c37653d10
Vitaliy Bezsheiko <vitaliybezsh@gmail.com>
2020-10-07 14:07:41 +0300

    Example of prediction with JS

--- index.js
+++ index.js
@@ -1,75 +1,151 @@
-const CIT_MAX_SEQUENCE_LENGTH = 150;
-const CIT_UNKNOWN_LABEL = 'unk';
-const CIT_PAD_LABEL = 'pad';
+CIT_MAX_SEQUENCE_LENGTH = 150;
+CIT_UNKNOWN_LABEL = 'unk';
+CIT_PAD_LABEL = 'pad';
 
-const pathToVocab = 'model4_js/vocab/word2idx.json';
-const pathToModel = 'http://localhost:63342/citParser/model4_js/model.json';
-
-function getRequest(path) {
-	return new Promise(function (resolve, reject) {
-		var oReq = new XMLHttpRequest();
-		oReq.open("GET", path);
-		let parsedJSON;
-		oReq.onload = function () {
-			parsedJSON = JSON.parse(this.responseText);
-			resolve(parsedJSON);
-		};
-		oReq.send();
-	});
-}
-
-function getModel(path) {
-	return new Promise(function (resolve, reject) {
-		 resolve(tf.loadLayersModel(path))
-	});
-}
-
-async function predict() {
-	const word2idx = await getRequest(pathToVocab);
-	const loadedModel = await getModel(pathToModel);
+class Prediction {
+	static PATH_TO_VOCAB = 'model4_js/vocab/word2idx.json';
+	static PATH_TO_MODEL = 'http://localhost:63342/citParser/model4_js/model.json';
+	static PATH_TO_LABELS = 'model4_js/vocab/idx2tag.json';
 	
-	var newData = [
-		'Baile W, Buckman R, Lenzi R, Glober G, Beale E, Kudelka A. SPIKES-A Six-Step Protocol for Delivering Bad News: Application to the Patient with Cancer. The Oncologist. 2000;5(4):302-11.',
-		'Patel K, Tatham K. Complete OSCE Skills for Medical and Surgical Finals. London: Hodder Arnold; 2010.',
-		'Burton N, Birdi K. Clinical Skills for OSCEs. London: Informa; 2006.'];
+	constructor(word2idx, idx2tag, loadedModel) {
+		this.word2idx = word2idx;
+		this.idx2tag = idx2tag;
+		this.loadedModel = loadedModel;
+	}
 	
-	var vectorized = [];
-	
-	newData.forEach(function (reference) {
-		var withoutPunctation = reference.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g, " ");
-		var wordArray = withoutPunctation.split(/\s+/);
-		wordArray = wordArray.filter(function (el) {
-			return el !== '';
+	static builder() {
+		return this.load().then(value => {
+			let {word2idx, idx2tag, loadedModel} = value;
+			return new Prediction(word2idx, idx2tag, loadedModel);
 		});
-		
-		var vectorizedSentence = [];
-		// Convert words to integers
-		wordArray.forEach(function (word) {
-			if (word2idx.hasOwnProperty(word)) {
-				vectorizedSentence.push(word2idx[word]);
-			} else {
-				vectorizedSentence.push(word2idx[CIT_UNKNOWN_LABEL])
-			}
+	}
+	
+	static getRequest(path) {
+		return new Promise(function (resolve, reject) {
+			let oReq = new XMLHttpRequest();
+			oReq.open("GET", path);
+			let parsedJSON;
+			oReq.onload = function () {
+				parsedJSON = JSON.parse(this.responseText);
+				resolve(parsedJSON);
+			};
+			oReq.send();
 		});
+	}
+	
+	static getModel(path) {
+		return new Promise(function (resolve, reject) {
+			resolve(tf.loadLayersModel(path));
+		});
+	}
+	
+	static async load() {
+		const word2idx = await this.getRequest(this.PATH_TO_VOCAB);
+		const idx2tag = await this.getRequest(this.PATH_TO_LABELS);
+		const loadedModel = await this.getModel(this.PATH_TO_MODEL);
+		return {word2idx, idx2tag, loadedModel};
+	}
+	
+	predict(data, word2idx, idx2tag, loadedModel) {
+		let tokenized = [];
+		let vectorized = [];
 		
-		// Fill with zeros
-		const sentenceLength = vectorizedSentence.length
-		if (sentenceLength < CIT_MAX_SEQUENCE_LENGTH) {
-			const arrayFill = new Array(CIT_MAX_SEQUENCE_LENGTH - sentenceLength).fill(word2idx[CIT_PAD_LABEL]);
-			arrayFill.forEach(function (value) {
-				vectorizedSentence.push(value)
+		data.forEach(function (reference) {
+			let withoutPunctation = reference.replace(/[\u2000-\u206F\u2E00-\u2E7F\\'!"#$%&()*+,\-.\/:;<=>?@\[\]^_`{|}~]/g, " ");
+			let wordArray = withoutPunctation.split(/\s+/);
+			wordArray = wordArray.filter(function (el) {
+				return el !== '';
 			});
-		}
+			
+			let tokenizedSentence = [];
+			let vectorizedSentence = [];
+			// Convert words to integers
+			wordArray.forEach(function (word) {
+				tokenizedSentence.push(word);
+				if (word2idx.hasOwnProperty(word)) {
+					vectorizedSentence.push(word2idx[word]);
+				} else {
+					vectorizedSentence.push(word2idx[CIT_UNKNOWN_LABEL])
+				}
+			});
+			
+			// Fill with zeros
+			const sentenceLength = vectorizedSentence.length
+			if (sentenceLength < CIT_MAX_SEQUENCE_LENGTH) {
+				const arrayFill = new Array(CIT_MAX_SEQUENCE_LENGTH - sentenceLength).fill(word2idx[CIT_PAD_LABEL]);
+				arrayFill.forEach(function (value) {
+					vectorizedSentence.push(value)
+				});
+			}
+			
+			tokenized.push(tokenizedSentence);
+			vectorized.push(vectorizedSentence);
+		});
+		
+		let sentenceLabelsPairs = new Map();
+		vectorized.forEach(function (sentence, index) {
+			// Make prediction and assign label IDs
+			let tensor = tf.tensor1d(sentence, 'int32').expandDims(0);
+			loadedModel.predict(tensor).argMax(-1).data().then(labelIds => {
+				// Create array with predicted labels
+				let labelTags = [];
+				const correspondentTokenizedSentence = tokenized[index];
+				labelIds.forEach(function (id, idIndex) {
+					if (idIndex > correspondentTokenizedSentence.length - 1) return;
+					labelTags.push(idx2tag[id]);
+				});
+				// Assign as word array - label tag array pair
+				sentenceLabelsPairs.set(correspondentTokenizedSentence, labelTags);
+			});
+		});
+		
+		return sentenceLabelsPairs;
+	}
+}
 
-		vectorized.push(vectorizedSentence);
-	});
+let prediction = Prediction.builder();
+
+async function makePrediction() {
+	let currentPrediction = await prediction;
 	
-	predictedLabels = [];
-	vectorized.forEach(function (sentence) {
-		let tensor = tf.tensor1d(sentence, dtype='int32').expandDims(0);
-		const predicted = loadedModel.predict(tensor).argMax(-1).data();
-		predictedLabels.push(predicted)
+	let citations = document.getElementById('citations').value;
+	if (!citations) return;
+	const data = citations.split(/\r\n|\r|\n/g);
+	return currentPrediction.predict(data, currentPrediction.word2idx, currentPrediction.idx2tag, currentPrediction.loadedModel);
+}
+
+function showPrediction() {
+	makePrediction().then(predicted => {
+		console.log(predicted);
+		let oldWrapper = document.getElementById('resultsWrapper');
+		if (oldWrapper) {
+			oldWrapper.remove();
+		}
+		
+		let resultsWrapperEl = document.createElement('main');
+		resultsWrapperEl.setAttribute('id', 'resultsWrapper');
+		document.body.appendChild(resultsWrapperEl);
+		
+		// TODO predicted should be defined without addition timeout, wondering what I'm missing in working with asynchronous functions
+		setTimeout(function () {
+			for (let [words, labels] of predicted.entries()) {
+				let tableEl = document.createElement('table');
+				resultsWrapperEl.appendChild(tableEl);
+				let labelsRowEl = document.createElement('tr')
+				let wordsRowEl = document.createElement('tr');
+				tableEl.appendChild(labelsRowEl);
+				tableEl.appendChild(wordsRowEl);
+				labels.forEach(function (value) {
+					let labelCellEl = document.createElement('td');
+					labelCellEl.append(value);
+					labelsRowEl.appendChild(labelCellEl);
+				});
+				words.forEach(function (value) {
+					let wordCellEl = document.createElement('td');
+					wordCellEl.append(value);
+					wordsRowEl.appendChild(wordCellEl);
+				});
+			}
+		}, 1000);
 	})
 }
-
-const predicted = predict();
